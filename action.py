a23f900cbca86a741aae5db74c0ae68656a633ad
btuladhar22 <btuladhar22@my.whitworth.edu>
2020-12-08 10:50:31 -0800

    draft

--- action.py
+++ action.py
@@ -1,4 +1,5 @@
 
+#create a action
 def createAction(mydb,ActionID,UserID,PostID,Time,ActionDescr):
     mycursor = mydb.cursor()
     sql = "INSERT INTO ACTION (ActionID,UserID,PostID,Time,ActionDescr) VALUES (%s,%s,%s,%s,%s)"
@@ -6,4 +7,15 @@ def createAction(mydb,ActionID,UserID,PostID,Time,ActionDescr):
 
     mycursor.execute(sql,val)
     mydb.commit()
+
+#Save a Post
+def savePost(mydb,UserId,PostID,ActionID,Time)
+    mycursor = my.cursor()
+    sql = "INSERT INTO ACTION (ActionID,UserID,PostID,Time,ActionDescr) VALUES (%s,%s,%s,%s,'saved post')"
+    val = (ActionID,UserID,PostID,Time,ActionDescr)
+
+    mycursor.execute(sql,val)
+    mydb.commit()
+
+
 
