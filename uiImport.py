56bc4e66eb421668c5db096a41a64e654da129bc
Phuc Hoang <chphuc05@gmail.com>
2020-12-10 10:14:01 -0800

    example dialog

--- uiImport.py
+++ uiImport.py
@@ -1,13 +1,30 @@
 from PyQt5 import QtWidgets, uic
 import sys
+from functools import partial
 
 class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, parent = None):
         super(Ui, self).__init__()
         uic.loadUi('login.ui', self)
         self.show()
+        self.LoginButton.clicked.connect(self.getLoginInfo)
+        self.registerAccount = Ui2(self)
+        #self.usernameLabel.setText('')
+
+    def getLoginInfo(self):
+        print("a")
+        self.registerAccount.show()
+
+class Ui2(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):
+        super(Ui2, self).__init__()
+        uic.loadUi('listivew.ui', self)
+        #self.show()
+        
 
         #self.usernameLabel.setText('')
+
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
